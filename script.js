8d48f12532a38ab054e99a7286d367a6de24f6ba
Nstoner <hsuw850@gmail.com>
2022-12-06 13:04:13 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+const list = document.querySelectorAll(".list");
+const homeId = document.getElementById("home");
+const profileId = document.getElementById("profile");
+const messageId = document.getElementById("message");
+const photosId = document.getElementById("photos");
+const settingId = document.getElementById("setting");
+const sliderTag = document.querySelector(".nav-indicator");
+const showTag = document.querySelectorAll(".show");
+const navListTag = document.querySelectorAll(".nav-list");
+
+function clickTagMenu () {
+    navListTag.forEach((item) => 
+    item.classList.remove("active"));
+    this.classList.add("active");
+    const clickedTagWidth = this.offsetWidth;
+    const clickedTagOffsetLeft = this.offsetLeft;
+    sliderTag.style.width = clickedTagWidth + "px";
+    sliderTag.style.transform = `translateX(${clickedTagOffsetLeft}px)`;
+    showTag.forEach((item) => item.classList.remove("active"));
+
+    navListTag[0].addEventListener("click", () => {
+        homeId.classList.add("active");
+    })
+    
+    navListTag[1].addEventListener("click", () => {
+        profileId.classList.add("active");
+    })
+    navListTag[2].addEventListener("click", () => {
+        messageId.classList.add("active");
+    })
+    navListTag[3].addEventListener("click", () => {
+        photosId.classList.add("active");
+    })
+    navListTag[4].addEventListener("click", () => {
+        settingId.classList.add("active");
+    })
+}
+
+navListTag.forEach((item) => item.addEventListener("click", clickTagMenu));
+
+
+function handleClick () {
+    
+    list.forEach((item) => 
+    item.classList.remove("active"));
+    this.classList.add("active");
+    showTag.forEach((item) => item.classList.remove("active"));
+
+    list[0].addEventListener("click", () => {
+        homeId.classList.add("active");
+    })
+    
+    list[1].addEventListener("click", () => {
+        profileId.classList.add("active");
+    })
+    list[2].addEventListener("click", () => {
+        messageId.classList.add("active");
+    })
+    list[3].addEventListener("click", () => {
+        photosId.classList.add("active");
+    })
+    list[4].addEventListener("click", () => {
+        settingId.classList.add("active");
+    })
+        
+}
+
+list.forEach((item) => 
+item.addEventListener("click", handleClick));
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+
+
